5ec45f725eedbac43eebde8626394754a547ba8e
romanMam <roman.mamrukov@gmail.com>
2023-09-20 11:23:23 +0100

    [Sample text client-side script]

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-// Smooth scrolling
+// Function to add a smooth scroll behavior
+function smoothScroll(targetId) {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// Attach click event handlers to navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-        });
+        smoothScroll(targetId);
     });
 });
 
-// Back to top button
+// Back to top button functionality
 const backToTopButton = document.getElementById('back-to-top');
 
+backToTopButton.addEventListener('click', () => {
+    smoothScroll('top'); // Replace 'home' with the ID of your top section
+});
+
+// Show/hide the back to top button based on scroll position
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
         backToTopButton.style.display = 'block';
@@ -23,10 +35,3 @@ window.addEventListener('scroll', () => {
         backToTopButton.style.display = 'none';
     }
 });
-
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
