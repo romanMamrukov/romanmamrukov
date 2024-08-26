780bf171b439c8ce35be56ba4670dd8fec2e0acc
Roman Mamrukov <roman.mamrukov@gmail.com>
2024-08-26 18:16:32 +0300

    update design, added footer

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.section');
     const burger = document.querySelector('.burger');
     const navLinks = document.querySelector('.nav-links');
+    const buttons = document.querySelectorAll('.filter-btn');
+    const items = document.querySelectorAll('.work-item');
 
     function updateNavbarColors(section) {
         if (section.classList.contains('about') || section.classList.contains('contact')) {
             navbar.style.backgroundColor = '#fff';
-            navbar.style.color = '#000';
+            navbar.style.color = '#0A6873';
             navLinks.style.backgroundColor = '#fff';
-            navbar.querySelectorAll('a').forEach(link => link.style.color = '#000');
-            burger.querySelectorAll('div').forEach(line => line.style.backgroundColor = '#000');
-            navLinks.querySelectorAll('a').forEach(line => line.style.color = '#000');
+            navbar.querySelectorAll('a').forEach(link => link.style.color = '#0A6873');
+            burger.querySelectorAll('div').forEach(line => line.style.backgroundColor = '#0A6873');
+            navLinks.querySelectorAll('a').forEach(line => line.style.color = '#0A6873');
         } else {
-            navbar.style.backgroundColor = '#000';
+            navbar.style.backgroundColor = '#043540';
             navbar.style.color = '#fff';
-            navLinks.style.backgroundColor = '#000';
+            navLinks.style.backgroundColor = '#043540';
             navbar.querySelectorAll('a').forEach(link => link.style.color = '#fff');
-            burger.querySelectorAll('div').forEach(line => line.style.backgroundColor = '#fff');
+            burger.querySelectorAll('div').forEach(line => line.style.backgroundColor = '#50F268');
             navLinks.querySelectorAll('a').forEach(line => line.style.color = '#fff');
         }
     }
@@ -49,4 +51,22 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 
-}); 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+          const filter = button.getAttribute('data-filter');
+    
+          // Remove active class from all buttons
+          buttons.forEach(btn => btn.classList.remove('active'));
+          // Add active class to the clicked button
+          button.classList.add('active');
+    
+          items.forEach(item => {
+            if (filter === 'all' || item.getAttribute('data-category') === filter) {
+              item.style.display = 'block';
+            } else {
+              item.style.display = 'none';
+            }
+          });
+        });
+      });
+});
